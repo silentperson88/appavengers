99c197996604f00a9d82834c170e880eef38f5a7
patel ravi <patelravi1102001@gmail.com>
2021-10-29 13:13:15 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
+// require("dotenv").config();
 const cors = require("cors");
 const profileManager = require("./profile/profileManager");
 
